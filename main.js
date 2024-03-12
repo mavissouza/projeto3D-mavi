ccd00ad8cc7a001c53fdd3b95729c8597cc52a40
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-03-12 10:11:03 -0300

    .

---
+++ main.js
@@ -0,0 +1,7 @@
+const botoes = document.querySelectorAll(".botao");
+
+for (let i=0; i < botoes.length; j++){
+    botoes[i].onclick = function (){
+        botoes[i].classList.add("ativo")
+    }
+}
