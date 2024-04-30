fc390be5ff71c146ca11942349995e17bae60b76
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-04-30 11:23:17 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i=0; i < botoes.length; i++){
-    botoes[i].onclick = function (){
-        for (let j = 0; j < botoes.length; j++){
+for (let i = 0; i<botoes.length; i++) {
+    botoes [i].onclick = function (){
+        for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -13,9 +13,9 @@ for (let i=0; i < botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("24-12-28T23:59:59");
-const tempoObjetivo2 = new Date("24-07-05T23:59:59");
-const tempoObjetivo3 = new Date("24-08-01T23:59:59");
-const tempoObjetivo4 = new Date("24-12-01T23:59:59");
+const tempoObjetivo2 = new Date("24-12-28T23:59:59");
+const tempoObjetivo3 = new Date("24-12-28T23:59:59");
+const tempoObjetivo4 = new Date("24-12-28T23:59:59");
 
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
@@ -26,9 +26,9 @@ for (let i=0; i< contadores.length; i++){
 }
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
-function calculaTempo(tempoObjetivo) {
+function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
@@ -38,8 +38,8 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias, "
-        + horas + " horas, "
-        + minutos + " minutos, "
-        + segundos + " segundos";
+    return dias + "dias"
+        + horas + "horas"
+        + minutos + "minutos"
+        + segundos + "segundos";
 }
