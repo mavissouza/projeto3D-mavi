e93e8e99fdfe1c7e8e7fdf9af24084b7e4eef0d7
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-04-30 10:36:19 -0300

    contadores

--- main.js
+++ main.js
@@ -12,4 +12,17 @@ for (let i=0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("24-12-28T23:59:59");
+const tempoObjetiv2 = new Date("24-07-05T23:59:59");
+const tempoObjetivo3 = new Date("24-08-01T23:59:59");
+const tempoObjetivo4 = new Date("24-12-01T23:59:59");
+
+const tempos = [
+    tempoObjetivo1, tempoObjetiv2, tempoObjetivo3, tempoObjetivo4
+]
+
+for (let i=0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
