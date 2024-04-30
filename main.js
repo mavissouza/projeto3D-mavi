714e510124391d497dc2402f22790ce5ea3841a7
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-04-30 11:33:02 -0300

    .

--- main.js
+++ main.js
@@ -16,9 +16,10 @@ const tempoObjetivo1 = new Date("24-12-28T23:59:59");
 const tempoObjetivo2 = new Date("24-12-28T23:59:59");
 const tempoObjetivo3 = new Date("24-12-28T23:59:59");
 const tempoObjetivo4 = new Date("24-12-28T23:59:59");
+const tempoObjetivo5 = new Date("25-04-12T23:59:59");
 
 const tempos = {
-    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+    tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo5
 }
 
 for (let i=0; i< contadores.length; i++){
