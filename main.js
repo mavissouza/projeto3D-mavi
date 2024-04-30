da9bbd8a1e4c06caede8bc62130f84ddf5e8fa33
Mavi <106076148+mavissouza@users.noreply.github.com>
2024-04-30 10:57:22 -0300

    .

--- main.js
+++ main.js
@@ -26,3 +26,11 @@ for (let i=0; i< contadores.length; i++){
 }
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+}
